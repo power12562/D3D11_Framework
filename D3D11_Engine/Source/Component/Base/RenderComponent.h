7bab9269c62b8b0dbba27509c11b48b1cacc0614
siwoo <power12562@gmail.com>
2024-10-04 11:59:42 +0900

    RenderComponent 와 Component 분리. RenderComponent는 Render 함수 존재. Component는 Render 함수 없음

---
+++ D3D11_Engine/Source/Component/Base/RenderComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Component\Base\Component.h>
+
+class RenderComponent : public Component
+{
+	friend class GameObject;
+public:
+	RenderComponent();
+	virtual ~RenderComponent() override;
+
+public:
+	virtual void Start() = 0; //초기화 함수는 명시적으로 호출 가능.
+protected:
+	virtual void FixedUpdate() = 0;
+	virtual void Update() = 0;
+	virtual void LateUpdate() = 0;
+	virtual void Render() = 0;
+};
