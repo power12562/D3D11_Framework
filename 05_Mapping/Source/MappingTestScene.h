f2ce40e551543cfbe64d9b2f3fed9f8ff6583acd
siwoo <power12562@gmail.com>
2024-10-07 17:14:08 +0900

    commit

--- 05_Mapping/Source/MappingTestScene.h
+++ 05_Mapping/Source/MappingTestScene.h
@@ -13,14 +13,5 @@ private:
 
 private:
 	float LightDir[4]{ 0, 0, 1, 0 };
-	float LightDiffuse[4]{ 1, 1, 1, 1 };
-	float LightAmbient[4]{ 0, 0, 0, 0 };
-	float LightSpecular[4]{ 1, 1, 1, 1 };
 
-	float MaterialAmbient[4]{ 1, 1, 1, 1 };
-	float MaterialDiffuse[4]{ 1, 1, 1, 1 };
-	float MaterialSpecular[4]{ 1, 1, 1, 1 };
-
-private:
-	float bgColor[4]{0,0,0,1};
 };
