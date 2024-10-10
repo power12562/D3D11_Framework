bdf7bbc5ef96c65ab362ce290a2146178ed5889c
siwoo <power12562@gmail.com>
2024-10-10 15:58:09 +0900

    줄 맞춤

--- D3D11_Engine/Source/Component/Base/RenderComponent.h
+++ D3D11_Engine/Source/Component/Base/RenderComponent.h
@@ -10,10 +10,10 @@ public:
 	virtual ~RenderComponent() override;
 
 public:
-	virtual void Start() = 0; //초기화 함수는 명시적으로 호출 가능.
+	virtual void Start()		= 0; //초기화 함수는 명시적으로 호출 가능.
 protected:
-	virtual void FixedUpdate() = 0;
-	virtual void Update() = 0;
-	virtual void LateUpdate() = 0;
-	virtual void Render() = 0;
+	virtual void FixedUpdate()	= 0;
+	virtual void Update()		= 0;
+	virtual void LateUpdate()	= 0;
+	virtual void Render()		= 0;
 };
