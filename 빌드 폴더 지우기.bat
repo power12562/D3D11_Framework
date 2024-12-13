c0c1e3d951e21f39982060b9d70a7a4037d9d941
siwoo <power12562@gmail.com>
2024-12-13 13:47:53 +0900

    commit

--- 빌드 폴더 지우기.bat
+++ 빌드 폴더 지우기.bat
@@ -3,11 +3,24 @@ chcp 65001
 
 echo [확인 단계] 삭제 대상 폴더를 검색 중입니다...
 
+:: 폴더를 찾았는지 여부 확인
+set found=0
+
 :: 삭제 대상 폴더 검색 및 출력
 for %%f in (.vs x64 x86 bin) do (
     for /d /r %~dp0 %%d in (%%f) do (
-        if exist "%%d" echo 삭제할 폴더: "%%d"
+        if exist "%%d" (
+		echo 삭제할 폴더: "%%d"
+		set found=1
+		 )
     )
+)
+
+:: 폴더를 찾지 못했을 경우 메시지 출력 후 종료
+if %found%==0 (
+    echo 찾을 수 있는 삭제 대상 폴더가 없습니다.
+    pause
+    exit /b
 )
 
 echo.
